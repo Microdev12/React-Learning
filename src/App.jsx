db1bad0b7d3487b6bf25491fedd87848871424ea
Microdev12 <singhaditya0407@gmail.com>
2025-05-04 23:53:31 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
+import { EXAMPLES } from './data.js'
 // import componentImage from "./assets/components.png";
 // import configImage from "./assets/config.png";
 // import jsxUIImage from "./assets/jsx-ui.png";
@@ -10,9 +12,27 @@ import TabButton from "./components/TabButton.jsx";
 // Object Destructing
 
 function App() {
-  function handlerClick() {
-    console.log("Hello Aditya!");
+
+  const [selectedTopic, setSelectedTopic] = useState()
+
+  function handlerClick(selectedValue) {
+    // TabContent = 'Hello This Tab Print '
+    setSelectedTopic(selectedValue)
+    console.log(selectedTopic)
   }
+  // Second Alternative Solution to render UI Conditionally
+  let tabContet = <p>Please select the topic</p>;
+
+  if(selectedTopic) {
+    tabContet = <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+  }
+  // Using Second Approach the code is bit cleaner 
 
   return (
     <div>
@@ -38,12 +58,21 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handlerClick}>Components</TabButton>
-            <TabButton onSelect={handlerClick}>JSX</TabButton>
-            <TabButton onSelect={handlerClick}>Props</TabButton>
-            <TabButton onSelect={handlerClick}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handlerClick('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handlerClick('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handlerClick('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handlerClick('state')}>State</TabButton>
           </menu>
+          {/* {!selectedTopic ?  <p>Please select the Topic</p> : <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>} */ tabContet}
+
         </section>
+
       </main>
     </div>
   );
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,10 @@
 
 /// as prop we can use object destructuring like {children, onSelect}
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, isSelected}) {
  
   return (
     <li>
-      <button onClick={onSelect}>{props.children}</button>
+      <button className={isSelected ? 'active' : ''} onClick={onSelect}>{children}</button>
     </li>
   );
 }
